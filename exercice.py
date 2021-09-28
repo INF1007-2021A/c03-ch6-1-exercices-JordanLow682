6d476c70cc57ac6f9a9f13a4f1fb0247a22a1513
DESKTOP-2NE278U\Jordan Low <jordan.low@polymtl.ca>
2021-09-28 11:14:16 -0400

    Exos 123

--- exercice.py
+++ exercice.py
@@ -3,23 +3,75 @@
 
 
 def order(values: list = None) -> list:
+    counter = 0
+    values_num = []
+    is_float = False
     if values is None:
-        # TODO: demander les valeurs ici
-        pass
+        values = []
+        while counter < 10:
+            print("Entrez la valeur #", counter + 1)
+            element = input()
+            values.append(element)
+            counter += 1
+    try:
+        float(values[0])
+        is_float = True
+    except ValueError:
+        is_float = False
 
-    return []
+    if is_float:
+        values_num = [float(element) for element in values]
+        values_num.sort()
+        print(values_num)
+        return values_num
+    else:
+        values.sort()
+        print(values)
+        return values
+        
 
 
 def anagrams(words: list = None) -> bool:
+    is_in_other = False
+    all_letters = []
     if words is None:
-        # TODO: demander les mots ici
-        pass
+        print("Entrez le premier mot:")
+        mot1 = input()
+        print("Entrez le deuxieme mot:")
+        mot2 = input()
+        words = [mot1, mot2]
+    
+    for letter in words[0]:
+        is_in_other = False
+        for letter1 in words[1]:
+            if letter == letter1:
+                is_in_other = True
+                all_letters.append(is_in_other)
+        if is_in_other == False:
+            all_letters.append(False)
+    
+    if all(all_letters):
+        print("C'est un anagram")
+    else:
+        print("Ce n'est pas un anagram")
 
     return False
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    items = [2, 6, 3, 7, 8, 0]
+    doublons = False
+    for i, element in enumerate(items):
+        for j, element1 in enumerate(items):
+            if element == element1 and i != j:
+                doublons = True
+    
+    if doublons:
+        print("Elle a des doublons")
+        return False
+    else:
+        print("Elle n'a pas de doublons")
+        return True
 
 
 def best_grades(student_grades: dict) -> dict:
